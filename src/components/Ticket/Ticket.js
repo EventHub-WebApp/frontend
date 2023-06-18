f234305667733fc92de467d2bfc354b0ea1f4bbb
alaasouabni <alaa.souabni@gmail.com>
2023-06-19 00:48:13 +0200

    fix bug in ticket

--- src/components/Ticket/Ticket.js
+++ src/components/Ticket/Ticket.js
@@ -115,8 +115,7 @@ const Ticket = () => {
     axios
       .get(process.env.REACT_APP_BASE_URL+`/events/${eventId}`)
       .then((res) => {
-        console.log(res.data);
-        setEvent(res.data);
+        setEvent(res.data.event);
         setIsLoading(false);
       })
       .catch((err) => {
